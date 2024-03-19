de2ac4fee8e95683487119b63ae214a57ade1a6f
eldi <eldirohmanur@gmail.com>
2024-03-19 23:49:40 +0700

    Initial commit

---
+++ app/src/main/java/com/eldi/homelauncher/OtherActivity.kt
@@ -0,0 +1,11 @@
+package com.eldi.homelauncher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OtherActivity: AppCompatActivity(R.layout.activity_ohter) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ other/src/main/java/com/eldi/other/OtherBActivity.kt
@@ -0,0 +1,12 @@
+package com.eldi.other
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OtherBActivity: AppCompatActivity(R.layout.other_b) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
